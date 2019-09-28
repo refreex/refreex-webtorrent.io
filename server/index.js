81c575ce485401c813a29c04815e9f5211b4756c
Lucas Recoaro <5874806+RecoX@users.noreply.github.com>
2019-09-28 15:46:11 +1200

    more tests to make the website work with heroku

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require('./rollbar')
 
 const compress = require('compression')
-const cors = require('cors')
+// const cors = require('cors')
 const express = require('express')
 const highlight = require('highlight.js')
 const http = require('http')
@@ -52,46 +52,46 @@ app.use(compress())
 // Use SSL
 app.use(function (req, res, next) {
   // Force SSL
-  if (config.isProd && req.protocol !== 'https') {
-    return res.redirect('https://' + (req.hostname || 'refreex.io') + req.url) // lgtm [js/server-side-unvalidated-url-redirection]
-  }
+  // if (config.isProd && req.protocol !== 'https') {
+  //   return res.redirect('https://' + (req.hostname || 'refreex.io') + req.url) // lgtm [js/server-side-unvalidated-url-redirection]
+  // }
 
-  // Redirect www to non-www
-  if (config.isProd && req.hostname === 'www.refreex.io') {
-    return res.redirect('https://refreex.io' + req.url) // lgtm [js/server-side-unvalidated-url-redirection]
-  }
+  // // Redirect www to non-www
+  // if (config.isProd && req.hostname === 'www.refreex.io') {
+  //   return res.redirect('https://refreex.io' + req.url) // lgtm [js/server-side-unvalidated-url-redirection]
+  // }
 
-  // Use HTTP Strict Transport Security
-  // Lasts 1 year, incl. subdomains, allow browser preload list
-  if (config.isProd) {
-    res.header(
-      'Strict-Transport-Security',
-      'max-age=31536000; includeSubDomains; preload'
-    )
-  }
+  // // Use HTTP Strict Transport Security
+  // // Lasts 1 year, incl. subdomains, allow browser preload list
+  // if (config.isProd) {
+  //   res.header(
+  //     'Strict-Transport-Security',
+  //     'max-age=31536000; includeSubDomains; preload'
+  //   )
+  // }
 
-  // Add cross-domain header for fonts, required by spec, Firefox, and IE.
-  const extname = path.extname(req.url)
-  if (['.eot', '.ttf', '.otf', '.woff'].indexOf(extname) >= 0) {
-    res.header('Access-Control-Allow-Origin', '*')
-  }
+  // // Add cross-domain header for fonts, required by spec, Firefox, and IE.
+  // const extname = path.extname(req.url)
+  // if (['.eot', '.ttf', '.otf', '.woff'].indexOf(extname) >= 0) {
+  //   res.header('Access-Control-Allow-Origin', '*')
+  // }
 
-  // Prevents IE and Chrome from MIME-sniffing a response. Reduces exposure to
-  // drive-by download attacks on sites serving user uploaded content.
-  res.header('X-Content-Type-Options', 'nosniff')
+  // // Prevents IE and Chrome from MIME-sniffing a response. Reduces exposure to
+  // // drive-by download attacks on sites serving user uploaded content.
+  // res.header('X-Content-Type-Options', 'nosniff')
 
-  // Prevent rendering of site within a frame.
-  res.header('X-Frame-Options', 'DENY')
+  // // Prevent rendering of site within a frame.
+  // res.header('X-Frame-Options', 'DENY')
 
-  // Enable the XSS filter built into most recent web browsers. It's usually
-  // enabled by default anyway, so role of this headers is to re-enable for this
-  // particular website if it was disabled by the user.
-  res.header('X-XSS-Protection', '1; mode=block')
+  // // Enable the XSS filter built into most recent web browsers. It's usually
+  // // enabled by default anyway, so role of this headers is to re-enable for this
+  // // particular website if it was disabled by the user.
+  // res.header('X-XSS-Protection', '1; mode=block')
 
-  // Force IE to use latest rendering engine or Chrome Frame
-  res.header('X-UA-Compatible', 'IE=Edge,chrome=1')
+  // // Force IE to use latest rendering engine or Chrome Frame
+  // res.header('X-UA-Compatible', 'IE=Edge,chrome=1')
 
-  next()
+  // next()
 })
 
 // Serve the demo torrent (Sintel)
